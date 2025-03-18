0408295e4543467e7f995bf2c34e52c1980b386d
Esty214778565 <8578397@gmail.com>
2025-03-18 14:48:05 +0200

    without connection string

--- Dotnet/Records.Data/DataContext.cs
+++ Dotnet/Records.Data/DataContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Humanizer;
+using Microsoft.EntityFrameworkCore;
 using Records.Core.Entities;
 using System;
 using System.Collections.Generic;
@@ -17,7 +18,9 @@ namespace Records.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
-            var connectionString = "server=b8jvuypcczfucowkes2u-mysql.services.clever-cloud.com;user=uqitu6fvq4ovwvks;password=3szKAbc6Bcm4woxsAye9;database=b8jvuypcczfucowkes2u";
+            //replace between the two connection strings
+            // var connectionString = "server=b8jvuypcczfucowkes2u-mysql.services.clever-cloud.com;user=uqitu6fvq4ovwvks;password=3szKAbc6Bcm4woxsAye9;database=b8jvuypcczfucowkes2u";
+            var connectionString = "";
             optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
            
             // optionsBuilder.UseSqlServer("server=b8jvuypcczfucowkes2u-mysql.services.clever-cloud.com;user=uqitu6fvq4ovwvks;password=3szKAbc6Bcm4woxsAye9;database=b8jvuypcczfucowkes2u");
