c351890d507f26e2dac64493f7fd9f25f4c42f2a
Esty214778565 <8578397@gmail.com>
2025-05-30 09:05:41 +0300

    ccxz

--- React/Client/vite-project/src/Components/VideoPlayer.tsx
+++ React/Client/vite-project/src/Components/VideoPlayer.tsx
@@ -128,21 +128,21 @@
 const VideoPlayer: React.FC<{ url: string; vttUrl: string }> = ({ url, vttUrl }) => {
     const videoUrl = `https://s3.amazonaws.com/my-first-records-bucket.testpnoren/${url}`;
     // Ensure vttUrl uses https
-    const safeVttUrl = vttUrl?.replace(/^http:\/\//, 'https://');
+    // const safeVttUrl = vttUrl?.replace(/^http:\/\//, 'https://');
 
     return (
         <div>
             <video width="600" controls>
                 <source src={videoUrl} type="video/mp4" />
-                {safeVttUrl && (
-                    <track
-                        kind="subtitles"
-                        srcLang="he"
-                        src={safeVttUrl}
-                        label="עברית"
-                        default
-                    />
-                )}
+
+                <track
+                    kind="subtitles"
+                    srcLang="he"
+                    src={"https://s3.amazonaws.com/my-first-records-bucket.testpnoren/" + vttUrl}
+                    label="עברית"
+                    default
+                />
+
                 הדפדפן שלך לא תומך בוידאו.
             </video>
         </div>
