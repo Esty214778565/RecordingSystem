654ccf00ea237e45e3cb47d5f2c789c62ccc1199
Esty214778565 <8578397@gmail.com>
2025-05-30 07:12:22 +0300

    try2

--- React/Client/vite-project/src/Reducers/LessonsSlice.tsx
+++ React/Client/vite-project/src/Reducers/LessonsSlice.tsx
@@ -2,8 +2,8 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { Lesson } from '../Models/Lesson';
 
-const apiUrl = "https://localhost:7043/api/record";
-// const apiUrl = "https://recordingsystem-server.onrender.com/api/record";
+// const apiUrl = "https://localhost:7043/api/record";
+const apiUrl = "https://recordingsystem-server.onrender.com/api/record";
 
 // Fetch lessons
 export const fetchLessons = createAsyncThunk('lessons/fetchLessons', async (_, thunkAPI) => {
